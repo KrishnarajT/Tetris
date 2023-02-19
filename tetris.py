0cb71b8382287b80061f8e316ae8e5f6e0c8f2e8
Krishnaraj Thadesar <krishnaraj.kpt@outlook.com>
2023-02-19 13:05:33 +0530

    added images

--- tetris.py
+++ tetris.py
@@ -5,6 +5,32 @@ from settings import *
 from tetromino import Tetromino
 
 
+class Text:
+    def __init__(self, app):
+        self.app = app
+        self.font = ft.Font(FONT_PATH)
+
+    def get_color(self):
+        time = pg.time.get_ticks() * 0.001
+        n_sin = lambda t: (math.sin(t) * 0.5 + 0.5) * 255
+        return n_sin(time * 0.5), n_sin(time * 0.2), n_sin(time * 0.9)
+
+    def draw(self):
+        self.font.render_to(self.app.screen, (WIN_W * 0.595, WIN_H * 0.02),
+                            text='TETRIS', fgcolor=self.get_color(),
+                            size=TILE_SIZE * 1.65, bgcolor='black')
+        self.font.render_to(self.app.screen, (WIN_W * 0.65, WIN_H * 0.22),
+                            text='next', fgcolor='orange',
+                            size=TILE_SIZE * 1.4, bgcolor='black')
+        self.font.render_to(self.app.screen, (WIN_W * 0.64, WIN_H * 0.67),
+                            text='score', fgcolor='orange',
+                            size=TILE_SIZE * 1.4, bgcolor='black')
+        self.font.render_to(self.app.screen, (WIN_W * 0.64, WIN_H * 0.8),
+                            text=f'{self.app.tetris.score}', fgcolor='white',
+                            size=TILE_SIZE * 1.8)
+
+
+
 class Tetris:
     def __init__(self, app):
         self.app = app
@@ -29,7 +55,10 @@ class Tetris:
                     self.field_array[row][x].alive = False
                     self.field_array[row][x] = 0
                     
-                
+    def is_game_over(self):
+        if any(self.field_array[0]):
+            pg.time.wait(1000)
+            return True
 
     def make_field_array(self):
         return [[0 for x in range(FIELD_WIDTH)] for y in range(FIELD_HEIGHT)]
@@ -40,6 +69,8 @@ class Tetris:
 
     def check_tetromino_landing(self):
         if self.tetromino.landed:
+            if self.is_game_over():
+                self.__init__(app=self.app)
             self.put_tetromino_in_field_array()
             self.tetromino = Tetromino(self)
             self.speed_up = False
